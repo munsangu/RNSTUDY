20ac3fa681ec9520aa0c54237d4e5dd658b724e7
munsangu <ds-3632@hanmail.net>
2022-03-01 23:49:50 +0900

    20220301

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,16 +1,32 @@
-import React, {useCallback} from 'react';
-import {Alert, Pressable, StyleSheet, Text, View} from 'react-native';
-import axios, {AxiosError} from 'axios';
-import Config from 'react-native-config';
-import {useAppDispatch} from '../store';
-import userSlice from '../slices/user';
-import {useSelector} from 'react-redux';
-import {RootState} from '../store/reducer';
-import EncryptedStorage from 'react-native-encrypted-storage';
+import React, { useCallback, useEffect } from "react";
+import { Alert, Pressable, StyleSheet, Text, View } from "react-native";
+import axios, { AxiosError } from "axios";
+import Config from "react-native-config";
+import { useAppDispatch } from "../store";
+import userSlice from "../slices/user";
+import { useSelector } from "react-redux";
+import { RootState } from "../store/reducer";
+import EncryptedStorage from "react-native-encrypted-storage";
 
 function Settings() {
   const accessToken = useSelector((state: RootState) => state.user.accessToken);
+  const money = useSelector((state: RootState) => state.user.money);
+  const name = useSelector((state: RootState) => state.user.name);
   const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    async function getMoney() {
+      const response = await axios.get<{ data: number }>(
+        `${Config.API_URL}/showmethemoney`,
+        {
+          headers: { authorization: `Bearer ${accessToken}` },
+        }
+      );
+      dispatch(userSlice.actions.setMoney(response.data.data));
+    }
+    getMoney();
+  }, [accessToken, dispatch]);
+
   const onLogout = useCallback(async () => {
     try {
       await axios.post(
@@ -20,17 +36,17 @@ function Settings() {
           headers: {
             Authorization: `Bearer ${accessToken}`,
           },
-        },
+        }
       );
-      Alert.alert('알림', '로그아웃 되었습니다.');
+      Alert.alert("알림", "로그아웃 되었습니다.");
       dispatch(
         userSlice.actions.setUser({
-          name: '',
-          email: '',
-          accessToken: '',
-        }),
+          name: "",
+          email: "",
+          accessToken: "",
+        })
       );
-      await EncryptedStorage.removeItem('refreshToken');
+      await EncryptedStorage.removeItem("refreshToken");
     } catch (error) {
       const errorResponse = (error as AxiosError).response;
       console.error(errorResponse);
@@ -39,13 +55,23 @@ function Settings() {
 
   return (
     <View>
+      <View style={styles.money}>
+        <Text style={styles.moneyText}>
+          {name}님의 수익금{" "}
+          <Text style={{ fontWeight: "bold" }}>
+            {money.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+          </Text>
+          원
+        </Text>
+      </View>
       <View style={styles.buttonZone}>
         <Pressable
           style={StyleSheet.compose(
             styles.loginButton,
-            styles.loginButtonActive,
+            styles.loginButtonActive
           )}
-          onPress={onLogout}>
+          onPress={onLogout}
+        >
           <Text style={styles.loginButtonText}>로그아웃</Text>
         </Pressable>
       </View>
@@ -54,22 +80,28 @@ function Settings() {
 }
 
 const styles = StyleSheet.create({
+  money: {
+    padding: 20,
+  },
+  moneyText: {
+    fontSize: 16,
+  },
   buttonZone: {
-    alignItems: 'center',
+    alignItems: "center",
     paddingTop: 20,
   },
   loginButton: {
-    backgroundColor: 'gray',
+    backgroundColor: "gray",
     paddingHorizontal: 20,
     paddingVertical: 10,
     borderRadius: 5,
     marginBottom: 10,
   },
   loginButtonActive: {
-    backgroundColor: 'blue',
+    backgroundColor: "blue",
   },
   loginButtonText: {
-    color: 'white',
+    color: "white",
     fontSize: 16,
   },
 });
